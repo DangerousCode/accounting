a467b175308d468aea9b92b2ac7fe7e818c06d9b
TCPSI\pajimenez <pedro.antonio.jimenez.gm@gmail.com>
2018-07-09 13:15:21 +0200

    cambio

--- src/main/java/com/ust/calc/calculadora/controllers/CalculadoraController.java
+++ src/main/java/com/ust/calc/calculadora/controllers/CalculadoraController.java
@@ -18,7 +18,6 @@ public class CalculadoraController implements Calculadora {
     @Override
     @RequestMapping("/calculo")
 	public Integer doCalculos(@RequestParam(value="one") Integer one, @RequestParam(value="two") Integer two) {
-		//return calculadoraService.doMultiply(one, two);
-    	return 1;
+		return calculadoraService.doMultiply(one, two);
 	}
 }
