84e90c904c8ba7c48083937885bf8ff0361b8628
Javier <javiergarciapuerta@gmail.com>
2018-07-09 15:03:38 +0200

    Se elimina código en MultiplicacionConsumer

--- src/main/java/com/ust/calc/calculadora/consumer/MultiplicacionConsumer.java
+++ src/main/java/com/ust/calc/calculadora/consumer/MultiplicacionConsumer.java
@@ -4,15 +4,6 @@ import org.springframework.web.client.RestTemplate;
 
 public class MultiplicacionConsumer {
 	
-	public static void main(String[] args) {
-		
-		MultiplicacionConsumer multiplicacionConsumer = new MultiplicacionConsumer();
-		int salida = multiplicacionConsumer.multiplicacion(3, 5);
-		System.out.println(salida);
-		
-	}
-	
-	
 	public int multiplicacion(int one, int two) {
 		
 		int respuesta = 0;
