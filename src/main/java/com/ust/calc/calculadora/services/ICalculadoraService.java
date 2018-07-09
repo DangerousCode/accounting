11ff33db3f05066d3e83b27cfc479b3b39faede1
TCPSI\pajimenez <pedro.antonio.jimenez.gm@gmail.com>
2018-07-09 16:11:24 +0200

    operaciones

--- src/main/java/com/ust/calc/calculadora/services/ICalculadoraService.java
+++ src/main/java/com/ust/calc/calculadora/services/ICalculadoraService.java
@@ -2,6 +2,9 @@ package com.ust.calc.calculadora.services;
 
 public interface ICalculadoraService {
 	
+	Integer doSum(Integer a, Integer b);
+	Integer doSubtraction(Integer a, Integer b);
 	Integer doMultiply(Integer a, Integer b);
+	Integer doDivide(Integer a, Integer b);
 
 }
--- src/main/java/com/ust/calc/calculadora/services/impl/CalculadoraServiceImpl.java
+++ src/main/java/com/ust/calc/calculadora/services/impl/CalculadoraServiceImpl.java
@@ -2,19 +2,46 @@ package com.ust.calc.calculadora.services.impl;
 
 import org.springframework.stereotype.Service;
 
-import com.ust.calc.calculadora.consumer.MultiplicacionConsumer;
+import com.ust.calc.calculadora.consumer.Consumer;
 import com.ust.calc.calculadora.services.ICalculadoraService;
 
 @Service
 public class CalculadoraServiceImpl implements ICalculadoraService {
 	
     @Override
-    public Integer doMultiply(Integer a, Integer b) {
+    public Integer doSum(Integer a, Integer b) {
     	
-    	MultiplicacionConsumer multiplicacionConsumer = new MultiplicacionConsumer();
+    	Consumer multiplicacionConsumer = new Consumer();
     	Integer salida = multiplicacionConsumer.multiplicacion(a, b);
     	
         return salida;
-    }	
+    }
+    
+    @Override
+    public Integer doSubtraction(Integer a, Integer b) {
+    	
+    	Consumer multiplicacionConsumer = new Consumer();
+    	Integer salida = multiplicacionConsumer.multiplicacion(a, b);
+    	
+        return salida;
+    }
+	
+    @Override
+    public Integer doMultiply(Integer a, Integer b) {
+    	
+    	Consumer multiplicacionConsumer = new Consumer();
+    	Integer salida = multiplicacionConsumer.multiplicacion(a, b);
+    	
+        return salida;
+    }
+    
+    @Override
+    public Integer doDivide(Integer a, Integer b) {
+    	
+    	Consumer multiplicacionConsumer = new Consumer();
+    	Integer salida = multiplicacionConsumer.multiplicacion(a, b);
+    	
+        return salida;
+    }    
 
 }
