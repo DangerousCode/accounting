851fc8afda5d99089886ed0c7d413125320136eb
Charly Chávez Ordóñez <nekperu15739@gmail.com>
2018-07-10 23:40:33 +0200

    Feature swagger pom.xml: added swagger and swagger-ui src/main/java/com/ust/calc/calculadora/config/SwaggerConfig.java: added swagger config src/main/java/com/ust/calc/calculadora/controllers/PaysheetController.java: added functional domain

--- src/main/java/com/ust/calc/calculadora/controllers/PaysheetController.java
+++ src/main/java/com/ust/calc/calculadora/controllers/PaysheetController.java
@@ -5,10 +5,12 @@ import com.ust.calc.calculadora.services.Payroll;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping(value = "/accounting")
 public class PaysheetController implements PaysheetAPI {
 
     private final Payroll<Paysheet, Paysheet> ustPayroll;
