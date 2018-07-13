1625fc7a43d4723e70bfea1ae98c2e1673205020
raulhsj <Raul.Hernandez@ust-global.com>
2018-07-13 14:36:44 +0200

    Test modified with real WS Client interface

--- src/test/java/com/ust/calc/calculadora/api/services/EmployeeCreationTest.java
+++ src/test/java/com/ust/calc/calculadora/api/services/EmployeeCreationTest.java
@@ -1,23 +1,20 @@
 package com.ust.calc.calculadora.api.services;
 
-import com.ust.calc.calculadora.api.resources.Employee;
-import com.ust.calc.calculadora.api.services.IEmployeeCreationService;
-import com.ust.calc.calculadora.api.services.impl.EmployeeCreationServiceImpl;
-import com.ust.calc.calculadora.clients.CalculadoraMultiplicacionClient;
-import com.ust.calc.calculadora.clients.entity.EmployeeDS;
-import com.ust.calc.calculadora.employee.consumers.EmployeeCreationWSClient;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-import java.math.BigDecimal;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Matchers.anyInt;
-import static org.mockito.Mockito.when;
+import com.ust.calc.calculadora.api.resources.Employee;
+import com.ust.calc.calculadora.api.services.impl.EmployeeCreationServiceImpl;
+import com.ust.calc.calculadora.clients.DataSourceClient;
+import com.ust.calc.calculadora.clients.entity.EmployeeDS;
 
 @RunWith(MockitoJUnitRunner.class)
 public class EmployeeCreationTest {
@@ -38,9 +35,10 @@ public class EmployeeCreationTest {
 
     @Test
     public void testEmployeeCreation() {
-    	final Employee outputService = new Employee();
-    	final EmployeeDS outputClientCall = new EmployeeDS();
-        when(employeeCreationWSClient.createEmployee(employee)).thenReturn(outputClientCall);
+    	Employee outputService = new Employee();
+    	final EmployeeDS inputClientCall = new EmployeeDS();
+    	final ResponseEntity<EmployeeDS> outputClientCall = new ResponseEntity<EmployeeDS>(HttpStatus.OK);	
+        when(employeeCreationWSClient.newEmployee(inputClientCall)).thenReturn(outputClientCall);
         outputService = employeeCreationService.createEmployee(employee);
         assertThat(outputService).isNotNull();
     }
