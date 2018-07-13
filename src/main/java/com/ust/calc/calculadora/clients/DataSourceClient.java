599bca96bd7e8a6365a1ac06b9753b4024815762
Javier <javiergarciapuerta@gmail.com>
2018-07-13 10:38:04 +0200

    DataSourceClient

---
+++ src/main/java/com/ust/calc/calculadora/clients/DataSourceClient.java
@@ -0,0 +1,16 @@
+package com.ust.calc.calculadora.clients;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.ust.calc.calculadora.clients.entity.EmployeeDS;
+
+@FeignClient(name="${feign.client.name}", url="${feign.client.url}")
+public interface DataSourceClient {
+
+	@RequestMapping(method = RequestMethod.POST, consumes="application/json",produces="application/json")
+	public ResponseEntity<EmployeeDS> newEmployee(EmployeeDS employeeDS);
+	
+}
