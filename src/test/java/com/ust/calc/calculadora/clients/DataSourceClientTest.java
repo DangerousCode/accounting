7bc43d57a7c93b84129128dfd736676760ca26e4
Javier <javiergarciapuerta@gmail.com>
2018-07-16 14:32:37 +0200

    modificaciones en el test

--- src/test/java/com/ust/calc/calculadora/clients/DataSourceClientTest.java
+++ src/test/java/com/ust/calc/calculadora/clients/DataSourceClientTest.java
@@ -2,49 +2,52 @@ package com.ust.calc.calculadora.clients;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.*;
+import static org.mockito.Matchers.isNotNull;
+import static org.mockito.Mockito.when;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.ust.calc.calculadora.clients.entity.EmployeeDS;
+import com.ust.calc.calculadora.clients.integration.IntegrationDSClient;
+import com.ust.calc.calculadora.clients.integration.impl.IntegrationDSClientImpl;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@RunWith(MockitoJUnitRunner.class)
 public class DataSourceClientTest {
 	
-	@Autowired
-	DataSourceClient dataSourceClient;
+	@Mock
+	private DataSourceClient dataSourceClient;
+	
+	@Mock EmployeeDS employeeDS;
+	
+	private IntegrationDSClient integrationDSClient;
+	
+	@Mock
+	private ResponseEntity<EmployeeDS> employeeDSResponse;
+	
+	@Mock
+	private EmployeeDS employeeResult;
+	
+	@Before
+    public void setUp() {
+        integrationDSClient = new IntegrationDSClientImpl(dataSourceClient);
+    }
 	
 	@Test
 	public void happyTest() throws JsonProcessingException {
 		
-		final EmployeeDS employeeDS = new EmployeeDS();
-		final ResponseEntity<EmployeeDS> httpResponse = dataSourceClient.newEmployee(employeeDS);
-		assertEquals(httpResponse.getStatusCode(), HttpStatus.CREATED);
-		
-	}
-	
-	@Test
-	public void nohappyTestRequestTimeout() throws JsonProcessingException {
-		
-		final EmployeeDS employeeDS = new EmployeeDS();
-		final ResponseEntity<EmployeeDS> httpResponse = dataSourceClient.newEmployee(employeeDS);
-		assertThat(httpResponse.getStatusCode()).isNotEqualByComparingTo(HttpStatus.REQUEST_TIMEOUT);
-		
-	}
-	
-	@Test
-	public void nohappyTestUnauthorized() throws JsonProcessingException {
-		
-		final EmployeeDS employeeDS = new EmployeeDS();
-		final ResponseEntity<EmployeeDS> httpResponse = dataSourceClient.newEmployee(employeeDS);
-		assertThat(httpResponse.getStatusCode()).isNotEqualByComparingTo(HttpStatus.UNAUTHORIZED);
+    	when(dataSourceClient.newEmployee(employeeDS)).thenReturn(employeeDSResponse);
+    	when(employeeDSResponse.getBody()).thenReturn(employeeResult);
+    	
+    	employeeResult = integrationDSClient.newEmployee(employeeDS);
+    	
+    	assertThat(employeeResult).isNotNull();
 		
 	}
 
