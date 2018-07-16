91ff681b423b52aafa835395b1aa169d0aac72aa
Javier <javiergarciapuerta@gmail.com>
2018-07-16 13:06:13 +0200

    DataSourceClientTest

--- src/test/java/com/ust/calc/calculadora/clients/DataSourceClientTest.java
+++ src/test/java/com/ust/calc/calculadora/clients/DataSourceClientTest.java
@@ -1,9 +1,8 @@
 package com.ust.calc.calculadora.clients;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.when;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +12,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ust.calc.calculadora.clients.entity.EmployeeDS;
 
 @RunWith(SpringRunner.class)
@@ -23,15 +21,30 @@ public class DataSourceClientTest {
 	@Autowired
 	DataSourceClient dataSourceClient;
 	
-	@Autowired
-	ObjectMapper mapper;
-	
 	@Test
 	public void happyTest() throws JsonProcessingException {
 		
 		final EmployeeDS employeeDS = new EmployeeDS();
 		final ResponseEntity<EmployeeDS> httpResponse = dataSourceClient.newEmployee(employeeDS);
 		assertEquals(httpResponse.getStatusCode(), HttpStatus.CREATED);
+		
+	}
+	
+	@Test
+	public void nohappyTestRequestTimeout() throws JsonProcessingException {
+		
+		final EmployeeDS employeeDS = new EmployeeDS();
+		final ResponseEntity<EmployeeDS> httpResponse = dataSourceClient.newEmployee(employeeDS);
+		assertThat(httpResponse.getStatusCode()).isNotEqualByComparingTo(HttpStatus.REQUEST_TIMEOUT);
+		
+	}
+	
+	@Test
+	public void nohappyTestUnauthorized() throws JsonProcessingException {
+		
+		final EmployeeDS employeeDS = new EmployeeDS();
+		final ResponseEntity<EmployeeDS> httpResponse = dataSourceClient.newEmployee(employeeDS);
+		assertThat(httpResponse.getStatusCode()).isNotEqualByComparingTo(HttpStatus.UNAUTHORIZED);
 		
 	}
 
