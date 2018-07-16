e54f3c68346371418780a9b47d4c44db5101015d
Javier <javiergarciapuerta@gmail.com>
2018-07-16 09:09:05 +0200

    DataSourceClientTest

--- src/test/java/com/ust/calc/calculadora/clients/DataSourceClientTest.java
+++ src/test/java/com/ust/calc/calculadora/clients/DataSourceClientTest.java
@@ -1,14 +1,14 @@
 package com.ust.calc.calculadora.clients;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -23,39 +23,15 @@ public class DataSourceClientTest {
 	@Autowired
 	DataSourceClient dataSourceClient;
 	
-	EmployeeDS employeeDS;
-	
 	@Autowired
 	ObjectMapper mapper;
-	
-	@Before
-	public void setUp() throws Exception {
-	}
 	
 	@Test
 	public void happyTest() throws JsonProcessingException {
 		
-		employeeDS = new EmployeeDS(null, "Raquel", null, null, null, null, null, null, null, null, null, null, null, null, null, null);
-		
-		ResponseEntity<EmployeeDS> resultado = dataSourceClient.newEmployee(employeeDS);
-		
-		EmployeeDS employee =resultado.getBody();
-		
-		assertEquals(employee.getName(), "Raquel");
-		
-	}
-	
-	@Test
-	public void nohappyTest() throws JsonProcessingException {
-		
-		employeeDS = new EmployeeDS(null, "Carlos", null, null, null, null, null, null, null, null, null, null, null, null, null, null);
-		
-		ResponseEntity<EmployeeDS> resultado = dataSourceClient.newEmployee(employeeDS);
-		
-		EmployeeDS employee =resultado.getBody();
-		
-		assertThat(employee.getName()).isNotEmpty();
-		
+		final EmployeeDS employeeDS = new EmployeeDS();
+		final ResponseEntity<EmployeeDS> httpResponse = dataSourceClient.newEmployee(employeeDS);
+		assertEquals(httpResponse.getStatusCode(), HttpStatus.CREATED);
 		
 	}
 
